044efec39db6173f44c91fa76e05b80acefc46cf
murdock33 <tomhagen849@gmail.com>
2023-11-09 08:07:34 +0700

    Week 1 Javascript Quiz - Functions

---
+++ part-7.5/3.js
@@ -0,0 +1,10 @@
+function processSentence(Name,Age,Address,Hobby) {
+    return `Nama saya ${Name}, umur saya ${Age} tahun, alamat saya di ${Address}, dan saya punya hobby yaitu ${Hobby}!`
+}
+
+let name = "Agus";
+let age = 30;
+let address = "Jln. Malioboro, Jogjakarta";
+let hobby = "gaming";
+
+console.log(processSentence(name,age,address,hobby))
