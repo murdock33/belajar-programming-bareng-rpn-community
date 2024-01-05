3984c1be467df28e9982f730d9aeb46be30467ff
murdock33 <tomhagen849@gmail.com>
2024-01-05 08:25:05 +0700

    Function Ubah Huruf

---
+++ part-9.2/5.js
@@ -0,0 +1,29 @@
+//sistem ubah hurufnya misal huruf a diubah menjadi b, c menjadi d, b menjadi c, z menjadi a
+//intinya ubah huruf menjadi huruf setelahnya
+function ubahHuruf(kata) {
+  // you can only write your code here!
+  let result = '';
+  let alphabet = "abcdefghijklmnopqrstuvwxyz";
+  let kataSplit = kata.split("");
+  let alphabetSplit = alphabet.split("");
+
+  for (let i = 0; i < kataSplit.length; i++) {
+    for (let j = 0; j < alphabetSplit.length; j++) {
+      if (kataSplit[i] === alphabetSplit[j]) {
+        if(kataSplit[i] == 'z') {
+            result += 'a';
+        } else {
+            result += alphabetSplit[j+1]
+        }
+      }
+    }
+  }
+  return result;
+}
+
+// TEST CASES
+console.log(ubahHuruf("wow")); // xpx
+console.log(ubahHuruf("developer")); // efwfmpqfs
+console.log(ubahHuruf("javascript")); // kbwbtdsjqu
+console.log(ubahHuruf("keren")); // lfsfo
+console.log(ubahHuruf("semangat")); // tfnbohbu
