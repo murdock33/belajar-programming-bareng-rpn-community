fbe14cab651996ceb41f469d3a65e8dbc4ac7803
murdock33 <tomhagen849@gmail.com>
2024-01-30 09:26:22 +0700

    Codewars(Kyu 6) -Create Phone Number

---
+++ codewars/phoneNumber.js
@@ -0,0 +1,15 @@
+// Create Phone Number - Kyu 6
+
+function createPhoneNumber(numbers) {
+  let format = "(xxx) xxx-xxxx";
+
+  for (let i = 0; i < numbers.length; i++) {
+    format = format.replace("x", numbers[i]);
+  }
+
+  return format;
+}
+
+console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
+console.log(createPhoneNumber([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]));
+console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
