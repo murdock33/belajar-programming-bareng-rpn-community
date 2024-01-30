4a90d641caace3a4b774b1c15544a445bc89fa26
murdock33 <tomhagen849@gmail.com>
2024-01-30 09:38:26 +0700

    cls

---
+++ codewars/listFiltering.js
@@ -0,0 +1,15 @@
+function filter_list(l) {
+  // Return a new array with the strings filtered out
+  let newArray = [];
+
+  for (let i = 0; i < l.length; i++) {
+    if (typeof l[i] === typeof(1)) {
+      newArray.push(l[i]);
+    }
+  }
+  return newArray;
+}
+
+console.log(filter_list([1, 2, "a", "b"]));
+console.log(filter_list([1, "a", "b", 0, 15]));
+console.log(filter_list([1, 2, "aasf", "1", "123", 123]));
